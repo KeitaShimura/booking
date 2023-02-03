a1a811d65a9fb2b5a19f270e2caa658c9870c0bd
KeitaShimura <keitashimura2023@gmail.com>
2023-02-04 06:17:47 +0900

    FullCalendarの導入

--- index.php
+++ index.php
@@ -6,3 +6,25 @@ while($booking = $bookings->fetch()) {
     print($booking['name']);
 }
 ?>
+
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.1/index.global.min.js'></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script>
+  </head>
+  <body>
+    <div id='calendar'></div>
+  </body>
+</html>
