9d630e2da8053a4be370a13125aaa0bc93604465
KeitaShimura <keitashimura2023@gmail.com>
2023-02-05 02:33:33 +0900

    非同期処理での住所自動入力 の変更

--- add_do.php
+++ add_do.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,10 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
     <div style="margin:0 auto; max-width: 1000px;">
-        <form method="post" action="add.php" style="margin: 100px;">
+        <form name="contact" method="post" action="add.php" style="margin: 100px;">
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">お名前</label>
                 <input type="text" class="form-control" id="name" name="name" placeholder="">
@@ -21,8 +23,6 @@
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">郵便番号</label>
                 <input type="text" class="form-control" id="post_code" name="post_code" placeholder="">
-                <button type="button" onclick="searchAddress ()">探す</button>
-                <div id="check"></div>
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">住所</label>
@@ -48,18 +48,17 @@
         </form>
     </div>
     <script>
-        async function searchAddress() {
-            const postalCode = document.getElementById("post_code").value;
-            const url = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode=' + postalCode;
-            const response = await fetch(url);
-            const data = await response.json();
-            if (!(data['results'])) {
-                document.getElementById("check").innerHTML = '郵便番号が存在しません';
-            } else {
-                const address = data['results'][0]['address1'] + data['results'][0]['address2'] + data['results'][0]['address3']
-                document.getElementById("address").value = address;
-            }
-        }
+        const contactForm = document.forms.contact;
+        contactForm.post_code.addEventListener('input', e => {
+            fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${e.target.value}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(e.target.value);
+                    contactForm.address.value = data.results[0].address1 + data.results[0].address2 + data.results[0].address3;
+                })
+                .catch(error => console.log(error))
+        })
     </script>
 </body>
+
 </html>
