7f58ce79728eebc155fb2dfc2411464b6895030a
KeitaShimura <keitashimura2023@gmail.com>
2023-02-05 01:25:40 +0900

    予約情報一覧画面へ遷移の実装

--- add.php
+++ add.php
@@ -4,5 +4,5 @@ require "db.php";
 $bookings = $pdo->prepare('INSERT INTO bookings SET name=?, phone=?, post_code=?, address=?, member=?, start=?, end=?, memo=?, created_at=NOW()');
 $bookings->execute(array($_POST['name'], $_POST['phone'], $_POST['post_code'], $_POST['address'], $_POST['member'], $_POST['start'], $_POST['end'], $_POST['memo']));
 
-header('Location: http://localhost/todo/');
+header('Location: http://localhost/booking/bookings.php');
 ?>
