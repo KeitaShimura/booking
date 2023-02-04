dff236cee31e4f2e379eec0935aa535c096a19f9
KeitaShimura <keitashimura2023@gmail.com>
2023-02-05 01:39:40 +0900

    予約情報一覧画面の作成

--- bookings.php
+++ bookings.php
@@ -17,36 +17,37 @@ $bookings = $pdo->query('SELECT * FROM bookings ORDER BY id DESC');
 </head>
 
 <body>
-    <h1 class="fs-1" style="margin: 50px 0 0 40px;">TODO一覧、登録画面</h1>
+    <h1 class="fs-1" style="margin: 50px 0 0 40px;">予約情報一覧</h1>
     <div style="text-align: center;" class="position-relative">
-        <form method="post" action="add.php">
-            <h2 class="fs-2" style="text-align: left; margin: 50px 0 0 10%;">TODOアプリ</h2>
-            <div class="form" style="text-align: center;">
-                <textarea name="content" cols="100" rows="4" placeholder="ここにTOTOを入力" style="width:80%; padding: 5px 10px;"></textarea>
-                <div class="button" style="margin-top: 5px;">
-                    <button type="submit" class="btn btn-primary">作成</button>
-                </div>
-            </div>
-        </form>
         <article>
-            <?php if ($posts->rowCount() === 0) { ?>
-                <h3 class="fs-3" style="text-align: center; margin: 50px 0 0 0;">TODOはありません</h3>
+            <?php if ($bookings->rowCount() === 0) { ?>
+                <h3 class="fs-3" style="text-align: center; margin: 50px 0 0 0;">予約はありません</h3>
             <?php } else { ?>
                 <div class="table-responsive">
                 <table class="table" style="margin:30px auto; text-align: center; border-top: 1px solid lightgray; width:80%;" >
                     <thead style="height: 50px;">
                         <tr>
-                            <th class="col-3" style="font-weight: bold;">メモの内容</th>
-                            <th class="col-3" style="font-weight: bold;">編集</th>
-                            <th class="col-3" style="font-weight: bold;">削除</th>
+                            <th class="" style="font-weight: bold;">お名前</th>
+                            <th class="" style="font-weight: bold;">電話番号</th>
+                            <th class="" style="font-weight: bold;">郵便番号</th>
+                            <th class="" style="font-weight: bold;">住所</th>
+                            <th class="" style="font-weight: bold;">人数</th>
+                            <th class="" style="font-weight: bold;">開始日</th>
+                            <th class="" style="font-weight: bold;">終了日</th>
+                            <th class="" style="font-weight: bold;">備考</th>
                         </tr>
                     </thead>
-                    <?php while ($post = $posts->fetch()) : ?>
+                    <?php while ($booking = $bookings->fetch()) : ?>
                         <tbody>
                             <tr>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($post['content']); ?></td>
-                                <td class="col-3" style="vertical-align: middle;"><a href="update_do.php?id=<?php print($post['id']); ?>" class="btn btn-primary">編集</a></td>
-                                <td class="col-3" style="vertical-align: middle;"><a href="delete.php?id=<?php print($post['id']); ?>" class="btn btn-danger">削除</a></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['name']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['phone']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['post_code']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['address']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['member']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['start']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['end']); ?></td>
+                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['memo']); ?></td>
                             </tr>
                         </tbody>
                     <?php endwhile; ?>
