2cc54a1201a008f318880ed8d4ae35af3315c8c2
KeitaShimura <keitashimura2023@gmail.com>
2023-02-05 20:56:23 +0900

    非同期処理での削除機能の実装

--- bookings.php
+++ bookings.php
@@ -14,6 +14,8 @@ $bookings = $pdo->query('SELECT * FROM bookings ORDER BY id DESC');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TODO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
 </head>
 
 <body>
@@ -24,40 +26,66 @@ $bookings = $pdo->query('SELECT * FROM bookings ORDER BY id DESC');
                 <h3 class="fs-3" style="text-align: center; margin: 50px 0 0 0;">予約情報はありません</h3>
             <?php } else { ?>
                 <div class="table-responsive">
-                <table class="table" style="margin:30px auto; text-align: center; border-top: 1px solid lightgray; width:80%;" >
-                    <thead style="height: 50px;">
-                        <tr>
-                            <th class="" style="font-weight: bold;">お名前</th>
-                            <th class="" style="font-weight: bold;">電話番号</th>
-                            <th class="" style="font-weight: bold;">郵便番号</th>
-                            <th class="" style="font-weight: bold;">住所</th>
-                            <th class="" style="font-weight: bold;">人数</th>
-                            <th class="" style="font-weight: bold;">開始日</th>
-                            <th class="" style="font-weight: bold;">終了日</th>
-                            <th class="" style="font-weight: bold;">備考</th>
-                            <th class="" style="font-weight: bold;">削除</th>
-                        </tr>
-                    </thead>
-                    <?php while ($booking = $bookings->fetch()) : ?>
-                        <tbody>
+                    <table class="table" style="margin:30px auto; text-align: center; border-top: 1px solid lightgray; width:80%;">
+                        <thead style="height: 50px;">
                             <tr>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['name']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['phone']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['post_code']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['address']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['member']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['start']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['end']); ?></td>
-                                <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['memo']); ?></td>
-                                <td class="col-3" style="vertical-align: middle;"><a href="delete.php?id=<?php print($booking['id']); ?>" class="btn btn-danger">削除</a></td>
+                                <th class="" style="font-weight: bold;">お名前</th>
+                                <th class="" style="font-weight: bold;">電話番号</th>
+                                <th class="" style="font-weight: bold;">郵便番号</th>
+                                <th class="" style="font-weight: bold;">住所</th>
+                                <th class="" style="font-weight: bold;">人数</th>
+                                <th class="" style="font-weight: bold;">開始日</th>
+                                <th class="" style="font-weight: bold;">終了日</th>
+                                <th class="" style="font-weight: bold;">備考</th>
+                                <th class="" style="font-weight: bold;">削除</th>
                             </tr>
-                            </tr>
-                        </tbody>
-                    <?php endwhile; ?>
-                </table>
+                        </thead>
+                        <?php while ($booking = $bookings->fetch()) : ?>
+                            <tbody>
+                                <tr>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['name']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['phone']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['post_code']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['address']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['member']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['start']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['end']); ?></td>
+                                    <td class="col-3" style="text-align: left; vertical-align: middle;"><?php print($booking['memo']); ?></td>
+                                    <!-- <td class="col-3" style="vertical-align: middle;"><a href="delete.php?id=<?php print($booking['id']); ?>" class="btn btn-danger">削除</a></td> -->
+                                    <td class="col-3" style="vertical-align: middle;"><a class="btn btn-danger" onclick="deleteData(<?php print($booking['id']); ?>)">削除</a></td>
+                                </tr>
+                            </tbody>
+                        <?php endwhile; ?>
+                    </table>
                 </div>
             <?php } ?>
         </article>
     </div>
+    <script>
+        function deleteData(id) {
+            var delete_url = "delete.php?id=" + id;
+            $.ajax({
+                    url: delete_url,
+                    type: "delete",
+                    data: {
+                        id: id,
+                    }
+                })
+                .done(function() {
+                    // var deleteContent = "#content" + id;
+
+                    // $(deleteContent).remove(); // 投稿削除
+                    clickEle.parents('tr').remove();
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    alert("error!");
+                    console.log("jqXHR          : " + jqXHR.status);
+                    console.log("textStatus     : " + textStatus);
+                    console.log("errorThrown    : " + errorThrown.message);
+                })
+            return true;
+        };
+    </script>
 </body>
+
 </html>
